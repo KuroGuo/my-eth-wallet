da6e715fd3ccaba94df925952183773607edeb26
Kuro <kuroguo@outlook.com>
2024-10-13 03:07:01 +0800

    Update hello2

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello world!!!</ThemedText>
+        <ThemedText type="title">Hello world 2</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>

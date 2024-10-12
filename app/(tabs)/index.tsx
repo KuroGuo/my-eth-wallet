933b728fb9f07dd3a211e79760e8c5814c7e5184
Kuro <kuroguo@outlook.com>
2024-10-13 03:04:37 +0800

    Update hello

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello world 2024</ThemedText>
+        <ThemedText type="title">Hello world!!!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>

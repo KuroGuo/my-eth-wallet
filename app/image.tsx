cdf94d9071d376eaf8853dfb4dd5ac6953f98144
Kuro <kuroguo@outlook.com>
2024-10-14 02:33:20 +0800

    Update

--- app/image.tsx
+++ app/image.tsx
@@ -15,13 +15,14 @@ export default function ImageScreen() {
       data={[
         'https://data.debox.pro/im/image/2024/10/13/6khmmmou/96aa8ba44cf7b8a858ae4e2927593904.jpg',
         'https://data.debox.pro/static/2023/03/16/120329_1678973015113.jpg',
-        'https://data.debox.pro/static/2023/12/21/oo0epp62/debox_christmas2023_1703141029261.png',
+        'https://data.debox.pro/im/image/2024/10/14/6khmmmou/2f789606d4940ead7e99eabe60d6bb686645b6f2fcaf8fb4be906d63306d2c35.jpg',
         'https://data.debox.pro/static/2023/07/4/fxi5qm2f/442056db2bed93eea27464b2c324f5b7.jpg',
         'https://data.debox.pro/static/2022/08/11/102785_-1945380697.jpeg'
       ]}
       doubleTapInterval={200}
       onTap={router.back}
       initialIndex={+params.id}
+      style={{ overflow: 'hidden' }}
     />
   </>
 }

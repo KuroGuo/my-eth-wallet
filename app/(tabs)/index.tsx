e65802852db818249e2e76b42c303767d1a8f9c3
Kuro <kuroguo@outlook.com>
2024-10-13 18:01:51 +0800

    Update

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -170,7 +170,7 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
-      <ThemedText>v 0.0.2</ThemedText>
+      <ThemedText>v 0.0.3</ThemedText>
     </ParallaxScrollView>
   );
 }

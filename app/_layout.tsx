2c82720b90e78fb2fa6fb7fb59c45a74ad197ac9
Kuro <kuroguo@outlook.com>
2024-10-13 22:46:28 +0800

    Add react-native-exception-handler

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,6 +12,25 @@ import Toast from 'react-native-toast-message'
 import Loading from '@/components/Loading'
 import { isExpoGo } from '@/app'
 
+import { setJSExceptionHandler } from 'react-native-exception-handler';
+import { Alert } from 'react-native'
+
+setJSExceptionHandler((e: any, isFatal: any) => {
+  if (isFatal) {
+    Alert.alert(
+      '未预期的错误发生',
+      `
+        Error: ${(isFatal) ? 'Fatal:' : ''} ${e.name} ${e.message}
+
+        我们将尝试恢复应用。
+      `,
+      [{ text: 'OK' }]
+    )
+  } else {
+    console.log(e) // 仅打印错误
+  }
+}, true)
+
 // // Prevent the splash screen from auto-hiding before asset loading is complete.
 // SplashScreen.preventAutoHideAsync();
 
